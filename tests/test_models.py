929825059ceb73d5413aba9615a875a9fdc5adcd
Kishan Patel <kishanpatel789@gmail.com>
2025-09-19 08:57:34 -0500

    Make article the default type

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,6 +11,13 @@ def set_up_database():
     SQLModel.metadata.create_all(engine)
 
 
+def save_to_db(artifact):
+    with Session(engine) as session:
+        session.add(artifact)
+        session.commit()
+        session.refresh(artifact)
+
+
 def test_create_artifact_model():
     tag_python = Tag(name="python")
     tag_cloud = Tag(name="cloud")
@@ -18,23 +25,18 @@ def test_create_artifact_model():
     artifact = Artifact(
         title="Test Article",
         url="https://example.com",
-        artifact_type=ArtifactTypeEnum.ARTICLE,
         notes="This seems interesting",
         tags=[tag_python, tag_cloud],
     )
+    save_to_db(artifact)
 
-    with Session(engine) as session:
-        session.add(artifact)
-        session.commit()
-        session.refresh(artifact)
-
-        assert artifact.id is not None
-        assert artifact.title == "Test Article"
-        assert artifact.url == "https://example.com"
-        assert artifact.artifact_type == ArtifactTypeEnum.ARTICLE
-        assert artifact.notes == "This seems interesting"
-        assert len(artifact.tags) == 2
-        assert artifact.content_raw is None
-        assert artifact.content_summary is None
-        assert artifact.created_at is not None
-        assert artifact.updated_at is None
+    assert artifact.id is not None
+    assert artifact.title == "Test Article"
+    assert artifact.url == "https://example.com"
+    assert artifact.artifact_type == ArtifactTypeEnum.ARTICLE
+    assert artifact.notes == "This seems interesting"
+    assert len(artifact.tags) == 2
+    assert artifact.content_raw is None
+    assert artifact.content_summary is None
+    assert artifact.created_at is not None
+    assert artifact.updated_at is None
