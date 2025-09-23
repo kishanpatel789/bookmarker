10eee62c7472c8eb006854e27d20e07c5f94c2c5
Kishan Patel <kishanpatel789@gmail.com>
2025-09-23 09:53:52 -0500

    Set up repository pattern for db

--- src/bookmarker/core/database.py
+++ src/bookmarker/core/database.py
@@ -1,17 +1,61 @@
-from contextlib import contextmanager
+from typing import Sequence
 
-from sqlmodel import Session, SQLModel, create_engine
+from sqlalchemy import Engine  # for typing
+from sqlmodel import Session, SQLModel, create_engine, select
 
 from .config import DATABASE_URL, DEBUG
-
-engine = create_engine(DATABASE_URL, echo=DEBUG)
+from .exceptions import ArtifactNotFoundError
+from .models import Artifact
 
 
 def create_db_and_tables():
+    engine = create_engine(DATABASE_URL, echo=DEBUG)
     SQLModel.metadata.create_all(engine)
 
 
-@contextmanager
-def get_session():
-    with Session(engine) as session:
-        yield session
+class DatabaseRepository:
+    def __init__(self, engine: Engine) -> None:
+        self._engine = engine
+
+    def add(self, artifact: Artifact) -> None:
+        with Session(self._engine) as session:
+            session.add(artifact)
+            session.commit()
+            session.refresh(artifact)
+
+    def list(self) -> Sequence[Artifact]:
+        with Session(self._engine) as session:
+            artifacts = session.exec(select(Artifact)).all()
+        return artifacts
+
+    def get(self, artifact_id: int) -> Artifact | None:
+        with Session(self._engine) as session:
+            artifact = session.get(Artifact, artifact_id)
+        return artifact
+
+    def get_by_url(self, url: str) -> Artifact | None:
+        with Session(self._engine) as session:
+            statement = select(Artifact).where(Artifact.url == url)
+            artifact = session.exec(statement).first()
+        return artifact
+
+    def delete(self, artifact_id: int) -> None:
+        with Session(self._engine) as session:
+            artifact = session.get(Artifact, artifact_id)
+            if artifact is not None:
+                session.delete(artifact)
+                session.commit()
+            else:
+                raise ArtifactNotFoundError(
+                    f"Artifact with ID {artifact_id} not found."
+                )
+
+    def store_content_raw(self, artifact_id: int, content: str) -> Artifact:
+        artifact = self.get(artifact_id)
+
+        with Session(self._engine) as session:
+            artifact.content_raw = content
+            session.add(artifact)
+            session.commit()
+            session.refresh(artifact)
+        return artifact
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,19 @@
 import pytest
 
+from src.bookmarker.core.database import DatabaseRepository, SQLModel, create_engine
 from src.bookmarker.core.main import add_artifact
 
 
-@pytest.mark.focus()
-def test_add_artifact():
-    artifact = add_artifact(title="Test Article", url="https://example.com")
+@pytest.fixture()
+def db_repo() -> DatabaseRepository:
+    engine = create_engine("sqlite:///:memory:", echo=True)
+    SQLModel.metadata.create_all(engine)
+    return DatabaseRepository(engine)
+
+
+def test_add_artifact(db_repo):
+    breakpoint()
+    artifact = add_artifact(db_repo, title="Test Article", url="https://example.com")
 
     assert artifact.id is not None
     assert artifact.title == "Test Article"
