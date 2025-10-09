88fbf11509d628273f5e87bc3eaf2eb0a40057d9
Kishan Patel <kishanpatel789@gmail.com>
2025-10-09 13:15:49 -0500

    Start initialization config workflow

--- src/bookmarker/cli/main.py
+++ src/bookmarker/cli/main.py
@@ -3,6 +3,7 @@ import typer
 from .base import app as base_app
 from .fetchers import app as fetchers_app
 from .helpers import app_callback
+from .init_config import app as init_config_app
 from .summarizers import app as summarizers_app
 
 app = typer.Typer(callback=app_callback)
@@ -10,3 +11,4 @@ app = typer.Typer(callback=app_callback)
 app.add_typer(base_app)
 app.add_typer(fetchers_app)
 app.add_typer(summarizers_app)
+app.add_typer(init_config_app)
